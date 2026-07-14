6d6a45296071ab1799af71c901b9a5db6c7ed4af
DennyMhrni <dennyirawan170204@gmail.com>
2026-07-14 11:20:35 +0700

    feat: add skripsi-filter configuration for managing active company keywords and hidden menu labels

--- src/configs/skripsi-filter.ts
+++ src/configs/skripsi-filter.ts
@@ -2,32 +2,36 @@ export const ACTIVE_COMPANY_KEYWORD = 'morindo';
 
 export const HIDDEN_MENU_LABELS = [
   // Master Data
-  'Akun',
-  'Grup Akun',
-  'Supplier',
-  'Customer',
-  'Tipe Unit',
-  'Kas',
-  'User',
+  'Sparepart',
+  // 'Akun',
+  // 'Grup Akun',
+  // 'Supplier',
+  // 'Customer',
+  // 'Tipe Unit',
+  // 'Kas',
+  // 'User',
   
 
 
   // Warehouse
-  'Penerimaan Unit',
-  'Pengeluaran Unit',
+  // 'Penerimaan Unit',
+  // 'Pengeluaran Unit',
   'Perlengkapan Masuk',
   'Perlengkapan Keluar',
   'Stock Perlengkapan',
   'Stok Perlengkapan', // in case of different spelling
+  'Penerimaan Material',
+  'Pengeluaran Material',
+  // 'Stok Material',
 
   // Laporan
-  'Laporan Pembelian',
-  'Laporan Penjualan',
-  'Laporan Penerimaan',
-  'Laporan Pengiriman',
-  'Laporan Warehouse',
+  // 'Laporan Pembelian',
+  // 'Laporan Penjualan',
+  // 'Laporan Penerimaan',
+  // 'Laporan Pengiriman',
+  // 'Laporan Warehouse',
 
   // Settings
-  'Roles',
-  'Permissions',
+  // 'Roles',
+  // 'Permissions',
 ];
